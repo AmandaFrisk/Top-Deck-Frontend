9e2383fe519c8e0e6da2cf0cb574808e13206cde
Melanny <melanny.lima@outlook.com>
2022-10-11 15:19:58 -0400

    final resolution

---
+++ src/components/nav1.js
@@ -0,0 +1,17 @@
+//This is imported in view-my-decks.js -AF
+import '../index.css';
+const Nav1 = () => {
+    return (
+        <nav className='nav1'>
+
+             <div className='logo-nav1-div'>
+
+                <img className ='logo-nav1'src='/images/Logo.png' alt='Top Deck Logo'/>
+            </div>
+
+
+        </nav>
+    )
+}
+
+export default Nav1
